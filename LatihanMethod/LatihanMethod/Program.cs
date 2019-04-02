495f48e8c9b31f3b9c968329216ac9d250d990ab
Indriana18 <48426491+Indriana18@users.noreply.github.com>
2019-04-02 14:35:38 +0700

    Add files via upload

---
+++ LatihanMethod/LatihanMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        
+            static void Main (string [] args )
+            {
+                //deklarasi variabel
+                int hasil;
+                Calculator calculator = new Calculator();
+
+                //membuat objek dari Class Calculator
+                hasil = calculator.Penjumlahan(10, 2);
+                CetakHasil("Hasil Penjumlahan : " + hasil);
+
+                hasil = calculator.Penjumlahan(10, 2, 3);
+                CetakHasil("Hasil penjumlahan overload :" + hasil);
+
+                hasil = calculator.Pengurangan(7, 2);
+                CetakHasil("Hasil Pengurangan " + hasil);
+
+                hasil = Calculator.Perkalian(5, 2);
+                CetakHasil("Hasil perkalian : " + hasil);
+
+                Console.ReadKey();
+                
+
+
+            }
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
